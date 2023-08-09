38e2ed37e66f0f75d7a91bef5acf693f7d3b9a9e
imakhlaq <imakhlaqXD@gmail.com>
2023-08-09 23:52:50 +0530

    DI

--- SpringBoot/01-DependancyInjection/src/main/java/in/ineuron/Application.java
+++ SpringBoot/01-DependancyInjection/src/main/java/in/ineuron/Application.java
@@ -1,13 +1,32 @@
 package in.ineuron;
 
+import in.ineuron.comp.WisMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        //container creation
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        //getting a bean
+        WisMessage wis = context.getBean(WisMessage.class);
+        wis.printDate();
     }
+
+
+    //Creating Bean of predefined classes
+    @Bean
+    public LocalDateTime injectTime() {
+        return LocalDateTime.now();
+    }
+
 
 }
---
+++ SpringBoot/01-DependancyInjection/src/main/java/in/ineuron/comp/WisMessage.java
@@ -0,0 +1,31 @@
+package in.ineuron.comp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class WisMessage {
+
+    /*
+
+    @Autowired
+    private LocalDateTime time;
+    */
+
+    private final LocalDateTime time;
+
+    //constructor injection of LocalDateTime Bean
+    @Autowired
+    WisMessage(LocalDateTime timme) {
+        this.time = timme;
+    }
+
+
+    public void printDate() {
+        System.out.println(time.getMonth());
+    }
+
+}
