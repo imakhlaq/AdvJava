ace32fd2fd95bb84e282377253ec821ec4dff082
imakhlaq <imakhlaqXD@gmail.com>
2023-08-22 23:54:15 +0530

    mongoDB

--- SpringBoot/springMVC/src/main/java/in/inuron/form/formData.java
+++ SpringBoot/springMVC/src/main/java/in/inuron/form/formData.java
@@ -1,7 +1,16 @@
 package in.inuron.form;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class formData {
+
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String getForm(Model model) {
+
+        return "form";
+    }
 }
